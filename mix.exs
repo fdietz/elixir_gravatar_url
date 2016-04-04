a6e68ca0673ba95ee19f8ee60a53f435f7a0c95c
Frederik Dietz <fdietz@gmail.com>
2016-04-04 20:25:56 +0200

    use semver version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirGravatarUrl.Mixfile do
 
   def project do
     [app: :elixir_gravatar_url,
-     version: "1.0",
+     version: "1.0.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
